8220e295139d5e436e4e20a049d0c919e0774c71
Joseph M. Temesi <joemunyendo@gmail.com>
2023-12-06 15:53:23 +0300

    Add comments

--- NODE JS BASICS/02_FileSystem/README.md
+++ NODE JS BASICS/02_FileSystem/README.md
@@ -1,2 +1,1 @@
 A Simple Node.js application that reads and outputs data stored in a different directory using fileSystem Module. The script also reads from a text file and writes to another file.
-A
--- NODE JS BASICS/02_FileSystem/app.js
+++ NODE JS BASICS/02_FileSystem/app.js
@@ -1,8 +1,8 @@
 const readline = require("readline");
-const fs = require("fs");
+const fs = require("fs");//import filesystem module
 
-let textData = fs.readFileSync("./files/input.txt", 'utf-8');
+let textData = fs.readFileSync("./files/input.txt", 'utf-8'); //method to read from specified file
 console.log(textData);
 
 let writeData = `Data read from input text: ${textData} \nDate Created: ${new(Date)}` 
-fs.writeFileSync("./files/inputData.txt", writeData);
+fs.writeFileSync("./files/inputData.txt", writeData);//method to write data in specified directory
